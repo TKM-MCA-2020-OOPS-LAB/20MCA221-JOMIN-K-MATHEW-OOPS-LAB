df1e40d44c48b6bba8810db0bb1c5ff648907a06
jominkmathew <jominachu001@gmail.com>
2021-06-10 10:41:53 +0530

    Lab

---
+++ CO4/prgrm2/Dimension.java
@@ -0,0 +1,14 @@
+package Graphics;
+
+public class Dimension {
+
+	public Dimension() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
